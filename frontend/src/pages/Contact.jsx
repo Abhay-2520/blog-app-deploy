4502d434c783bdd0ac6faf530565f1ec4b056d1c
Abhay Singh Sengar <abhaysinghsengar208@gmail.com>
2026-07-04 00:02:19 +0530

    Update Contact.jsx

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -27,7 +27,7 @@ function Contact() {
     }
   };
   return (
-    <div>
+    <div className = "text-black" >
       <div className="bg-gray-50 min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-4xl w-full space-y-8 bg-white p-10 rounded-lg shadow-lg">
           <div className="text-center">
@@ -99,11 +99,11 @@ function Contact() {
               <ul className="space-y-4">
                 <li className="flex items-center space-x-2">
                   <FaPhone className="text-red-500" />
-                  <span>+91 9876543210</span>
+                  <span>+91 98074*****</span>
                 </li>
                 <li className="flex items-center space-x-2">
                   <FaEnvelope className="text-pink-500" />
-                  <span>help@learncoding.com</span>
+                  <span>abhaysinghsengar5002@gmail.com.com</span>
                 </li>
                 <li className="flex items-center space-x-2">
                   <FaMapMarkerAlt className="text-green-500" />
